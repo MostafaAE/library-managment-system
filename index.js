f8c0620dd792f0c66fdf24d7e5cafba3282d8eb4
MahaElomey <mahaelkomey@gmail.com>
2022-03-18 22:01:23 +0200

    add members endpoint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =  require('express')
 const BooksList = require('./books-controller')
+const memberslist = require("./members-controller");
+
 
 const app = express()
 app.use(express.json())
@@ -10,6 +12,9 @@ app.get('/books',BooksList.getAllBooks);
 app.get('/books/:bookId',BooksList.getBookByID);
 app.post('/books',BooksList.addBook); 
 
+
+// members End Points
+app.post("/members", memberslist.addMember);
 
 // PORT
 app.listen(8080, ()=> console.log("Server is running on http://localhost:8080"))
--- members-controller.js
+++ members-controller.js
@@ -6,3 +6,21 @@ let membesrDB = [
       employeeAge: "23",
     },
   ];
+
+
+  // add member to members list
+  const addMember = (req,res)=>{
+   const member ={
+    memberId: memberDB.length+1,
+    memberName:req.body.memberName,
+    memberPhone:req.body.memberPhone,
+    employeeAge:req.body.employeeAge,
+  };
+  memberDB.push(member);
+  res.send(member);
+};
+
+
+module.exports={
+  addMember,
+};
