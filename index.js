975cedcb98539457e981670a4107b2c05a347fe0
MayarHanafy <mayar.hanafy.14@gmail.com>
2022-03-18 20:37:03 +0200

    get members endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =  require('express')
 const BooksList = require('./books-controller')
+const memberslist = require("./members-controller");
 
 const app = express()
 app.use(express.json())
@@ -10,6 +11,10 @@ app.get('/books',BooksList.getAllBooks);
 app.get('/books/:bookId',BooksList.getBookByID);
 app.post('/books',BooksList.addBook); 
 
+
+// members End Points
+app.get("/members", memberslist.getAllMembers);
+app.get("/members/:memberId", memberslist.getMemberbyId);
 
 // PORT
 app.listen(8080, ()=> console.log("Server is running on http://localhost:8080"))
--- members-controller.js
+++ members-controller.js
@@ -6,3 +6,22 @@ let membesrDB = [
       employeeAge: "23",
     },
   ];
+
+ 
+  // get all members
+  const getAllMembers = (req, res) => {
+    res.send(memberDB);
+ };
+
+
+ // get member by using id 
+ const getMemberbyId = (req, res) => {
+   let member = memberDB.find(mem => mem.memberId === parseInt(req.params.memberId));
+   if (!member) return res.status(404).send("the member with the given id not found");
+   res.send(member);
+ };
+
+ module.exports = {
+  getAllMembers,
+  getMemberbyId,
+};
