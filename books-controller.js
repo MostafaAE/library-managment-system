18c575adb8dcc185f2e20d6a9ad9114338b9db53
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-03-18 17:57:38 +0200

    get books end point

--- books-controller.js
+++ books-controller.js
@@ -1,7 +1,33 @@
 let booksDB = [
-    {
-      bookId: 1,
-      bookName: "The Alchemist",
-      bookISBN: "127-127-127-127",
-    },
-  ];
+  {
+    bookId:1,
+    bookName: "The Alchemist",
+    bookISBN: "127-127-127-127",
+  },
+  {
+    bookId: 2,
+    bookName: "The A Little Life",
+    bookISBN: "128-157-923-102",
+  },
+];
+
+
+//get all books given route to books
+const getAllBooks=(req,res)=>{
+res.send(booksDB);
+};
+
+
+//get book give id in param bookId
+const getBookByID =(req,res)=>
+{
+let BookByID =booksDB.find((book)=>book.bookId===parseInt(req.params.bookId))
+if(!BookByID) return res.status(404).send('The book with the given id not found');
+res.send(BookByID);
+}
+
+
+module.exports={
+  getAllBooks,
+  getBookByID
+};
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express =  require('express')
+const BooksList = require('./books-controller')
 
 const app = express()
 app.use(express.json())
 
 // End Points
-
+//Books end points
+app.get('/books',BooksList.getAllBooks);
+app.get('/books/:bookId',BooksList.getBookByID);
 
 // PORT
 app.listen(8080, ()=> console.log("Server is running on http://localhost:8080"))
